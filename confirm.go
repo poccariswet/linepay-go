7252536ddfe6626b49ae2a2bbabe0dd83dac5e16
soeyusuke <s1240231@gmail.com>
2018-10-15 13:40:02 +0900

    [FIX] confirm method

---
+++ confirm.go
@@ -0,0 +1,32 @@
+package linepay
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/pkg/errors"
+)
+
+type Confirmation struct {
+	Amount   uint64 `json:"amount"`
+	Currency string `json:"currency"`
+}
+
+func (pay *LinePay) Confirm(transactionID int, confirmation Confirmation) error {
+	url := fmt.Sprintf("https://%s/%s/payments/%d/confirm", SandboxApiHostname, ApiVersion, transactionID)
+	fmt.Println(url)
+
+	data, err := json.Marshal(confirmation)
+	if err != nil {
+		return errors.Wrap(err, "json marshal err")
+	}
+
+	respBody, err := pay.Post(url, data)
+	if err != nil {
+		return errors.Wrap(err, "linepay Post err")
+	}
+	log.Println(string(respBody))
+
+	return nil
+}
