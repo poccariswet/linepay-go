62f5320bde132e815bac30088ee3204a768dcc1e
soeyusuke <s1240231@gmail.com>
2018-10-15 13:06:01 +0900

    [ADD] post logic

--- linepay.go
+++ linepay.go
@@ -1,5 +1,13 @@
 package linepay
 
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/pkg/errors"
+)
+
 type LinePay struct {
 	ChannelID     string
 	ChannelSecret string
@@ -18,4 +26,26 @@ func New(channelID, channelSecret string) *LinePay {
 		ChannelSecret: channelSecret,
 		IsSandBox:     true,
 	}
+}
+
+func (pay *LinePay) Post(url string, data []byte) ([]byte, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	req.Header.Set("X-LINE-ChannelId", pay.ChannelID)
+	req.Header.Set("X-LINE-ChannelSecret", pay.ChannelSecret)
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "client do err")
+	}
+	defer resp.Body.Close()
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrap(err, "readall err")
+	}
+
+	return respBody, nil
 }
